192cfe25ab887b2a7e9ed04df915e595efa9046f
TanninOne <seppleviathan@gmx.de>
2018-10-18 08:49:07 +0200

    no longer delay loading of nbind
    this is addressing a problem we're experiencing in Vortex where delayed module imports (very occasionally) fail when the application is bundled with webpack.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 const path = require('path');
+const nbind = require('nbind');
 
 let winAcl = (() => {
   let lib;
   return () => {
     if (lib === undefined) {
-      lib = require('nbind').init(path.join(__dirname)).lib;
+      lib = nbind.init(path.join(__dirname)).lib;
     }
     return lib;
   }
